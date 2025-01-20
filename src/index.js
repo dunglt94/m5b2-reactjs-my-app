58bfd979fe01c416aa613fb7bf66f68a9f2e0fcf
dunglt94 <tuandung9594@gmail.com>
2025-01-20 12:01:23 +0700

    add bootstrap login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -87,6 +88,38 @@ root2.render(
                     <button className="btn third">FOLLOW</button>
                 </div>
             </div>
+        </div>
+    </div>
+);
+
+const root3 = ReactDOM.createRoot(document.getElementById("root3"));
+
+root3.render(
+    <div className="container1 d-flex align-items-center text-center">
+        <div className="form-signin">
+            <form>
+                <img className="mb-4"
+                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png"
+                     alt="" width="72" height="57"/>
+                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                <div className="form-floating">
+                    <input type="email" className="form-control email" id="floatingInput"
+                           placeholder="name@example.com"/>
+                    <label>Email address</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control password" id="floatingPassword"
+                           placeholder="Password"/>
+                    <label>Password</label>
+                </div>
+                <div className="checkbox mb-3">
+                    <label>
+                        <input type="checkbox"/> Remember me
+                    </label>
+                </div>
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
+            </form>
         </div>
     </div>
 );
