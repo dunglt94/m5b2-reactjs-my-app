30bd98037a947bf0af3698a7e835a0b04fda78da
dunglt94 <tuandung9594@gmail.com>
2025-01-20 11:48:47 +0700

    add student list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -8,3 +9,60 @@ root.render(
         Browser's details: {navigator.userAgent}
     </h3>
 );
+
+const students = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+]
+
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+
+root1.render(
+    <table>
+        <thead>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+        </thead>
+        <tbody>
+        {students.map(student => (
+            <tr>
+                <td>{student.company}</td>
+                <td>{student.contact}</td>
+                <td>{student.country}</td>
+            </tr>
+        ))}
+        </tbody>
+    </table>
+);
+
